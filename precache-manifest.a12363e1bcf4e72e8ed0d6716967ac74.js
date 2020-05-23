6e1a0fdb3c0f7ec65d44ea6ef1451301dcad8df6
Manish Bhattarai <bhaimanish00@gmail.com>
2020-05-23 14:16:48 +0545

    Updates

---
+++ precache-manifest.a12363e1bcf4e72e8ed0d6716967ac74.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "d2eaceab1740352b2b84",
+    "url": "/shoppingcart/static/css/main.c7dc66f2.chunk.css"
+  },
+  {
+    "revision": "d2eaceab1740352b2b84",
+    "url": "/shoppingcart/static/js/main.d2eaceab.chunk.js"
+  },
+  {
+    "revision": "a0e460131e19bc08d184",
+    "url": "/shoppingcart/static/js/1.a0e46013.chunk.js"
+  },
+  {
+    "revision": "947db2b407c85a628daa",
+    "url": "/shoppingcart/static/js/runtime~main.947db2b4.js"
+  },
+  {
+    "revision": "6c426583a257819912278cedfdd9badf",
+    "url": "/shoppingcart/static/media/item1.6c426583.jpg"
+  },
+  {
+    "revision": "8779dd15782203466bfb67cac99ef2ec",
+    "url": "/shoppingcart/static/media/item2.8779dd15.jpg"
+  },
+  {
+    "revision": "9bd2546f523e4a6e2515068c65ca441c",
+    "url": "/shoppingcart/static/media/item3.9bd2546f.jpg"
+  },
+  {
+    "revision": "65bc33d70f7f4ca793c5acc24525de9f",
+    "url": "/shoppingcart/static/media/item4.65bc33d7.jpg"
+  },
+  {
+    "revision": "f5fd97b56137c3ed5df0f3f71482c105",
+    "url": "/shoppingcart/static/media/item6.f5fd97b5.jpg"
+  },
+  {
+    "revision": "7cf26928e9ca1ef341ea5639fbce92f6",
+    "url": "/shoppingcart/index.html"
+  }
+];
