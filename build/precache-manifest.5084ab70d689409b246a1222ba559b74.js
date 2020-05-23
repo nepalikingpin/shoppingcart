4fef038578ad333bddb44131e252e6004aedfdb7
Manish Bhattarai <bhaimanish00@gmail.com>
2020-05-23 14:07:01 +0545

    Add files via upload

---
+++ build/precache-manifest.5084ab70d689409b246a1222ba559b74.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "d2eaceab1740352b2b84",
+    "url": "/static/css/main.c7dc66f2.chunk.css"
+  },
+  {
+    "revision": "d2eaceab1740352b2b84",
+    "url": "/static/js/main.d2eaceab.chunk.js"
+  },
+  {
+    "revision": "a0e460131e19bc08d184",
+    "url": "/static/js/1.a0e46013.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "6c426583a257819912278cedfdd9badf",
+    "url": "/static/media/item1.6c426583.jpg"
+  },
+  {
+    "revision": "8779dd15782203466bfb67cac99ef2ec",
+    "url": "/static/media/item2.8779dd15.jpg"
+  },
+  {
+    "revision": "9bd2546f523e4a6e2515068c65ca441c",
+    "url": "/static/media/item3.9bd2546f.jpg"
+  },
+  {
+    "revision": "65bc33d70f7f4ca793c5acc24525de9f",
+    "url": "/static/media/item4.65bc33d7.jpg"
+  },
+  {
+    "revision": "f5fd97b56137c3ed5df0f3f71482c105",
+    "url": "/static/media/item6.f5fd97b5.jpg"
+  },
+  {
+    "revision": "01507433ae3f4f9a53f38e38b6c18d87",
+    "url": "/index.html"
+  }
+];
